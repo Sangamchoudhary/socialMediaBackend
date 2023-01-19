6cc18cc3b0de4eb59bce201cab1e9c3d7f61ba5f
Sangam choudhary <Sangamchoudhary7@gmail.com>
2023-01-19 14:24:10 +0530

    add landing page details

--- myApp/controller/landingPageController.js
+++ myApp/controller/landingPageController.js
@@ -1,5 +1,8 @@
 module.exports.landingPage = function landingPage(req, res) {
-  return res.send(
-    "Welcome to social media backend ! Try out different API mentioned in github repo"
-  );
+  return res.json({
+    "Welcome note" : "Welcome to social media backend ! Try out different API mentioned in github repo",
+    "start with login" : "make a post request on this API https://socialmediabackendassignment.onrender.com/api/authenticate/login",
+    "email" : "admin1@admin.com",
+    "password" : "admin1"
+  });
 };
